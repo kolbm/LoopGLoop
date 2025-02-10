77e1c74d7d5db31096905d55b948646f6c6071d1
Marc Kolb <kolbm@dunellenschools.org>
2025-02-10 16:25:24 -0500

    Update app.py

--- app.py
+++ app.py
@@ -128,11 +128,9 @@ if st.button("Find Paragraph", key="find_paragraph_button") and entry_number is 
         video_info = video_mapping.get((location_code, entry_number))
         if video_info:
             video_id, start_time, end_time = video_info
-            st.markdown(f"""
-            <iframe width="560" height="315" 
-            src="https://www.youtube.com/embed/{video_id}?start={start_time}&controls=1" 
-            frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
-            """, unsafe_allow_html=True)
+            if video_info:
+            video_id, start_time, end_time = video_info
+            st.video(f"https://www.youtube.com/watch?v={video_id}")
         
         st.subheader("Matching Location")
         st.write(f"<p class='narrative-text'>{result.iloc[0]['Location']}</p>", unsafe_allow_html=True)
