93bcaaab34458328caf6e33bbf1931c6e638af32
Marc Kolb <kolbm@dunellenschools.org>
2025-02-13 10:48:26 -0500

    VT correction

--- app.py
+++ app.py
@@ -44,28 +44,28 @@ calculation_type = st.sidebar.selectbox("What would you like to solve for:", ["C
 g = 10  # Gravity (m/s²)
 
 if calculation_type == "Centripetal Acceleration":
-    st.latex(r"a_c = \frac{v^2}{r}")
+    st.latex(r"a_c = \frac{{v_T}^2}{r}")
     radius = st.sidebar.number_input("Radius (m)", min_value=0.1, value=5.0, step=0.1)
     velocity = st.sidebar.number_input("Tangential Velocity (m/s)", min_value=0.0, value=5.0, step=0.1)
     acceleration = calculate_centripetal_acceleration(velocity, radius)
     st.write(f"Centripetal Acceleration: {acceleration:.2f} m/s²")
 
 elif calculation_type == "Tangential Velocity":
-    st.latex(r"v = \sqrt{a_c \cdot r}")
+    st.latex(r"{v_T} = \sqrt{a_c \cdot r}")
     acceleration = st.sidebar.number_input("Centripetal Acceleration (m/s²)", min_value=0.1, value=5.0, step=0.1)
     radius = st.sidebar.number_input("Radius (m)", min_value=0.1, value=5.0, step=0.1)
     velocity = calculate_tangential_velocity(acceleration, radius)
     st.write(f"Tangential Velocity: {velocity:.2f} m/s")
 
 elif calculation_type == "Radius of the Loop":
-    st.latex(r"r = \frac{v^2}{a_c}")
+    st.latex(r"r = \frac{{v_T}^2}{a_c}")
     velocity = st.sidebar.number_input("Tangential Velocity (m/s)", min_value=0.0, value=5.0, step=0.1)
     acceleration = st.sidebar.number_input("Centripetal Acceleration (m/s²)", min_value=0.1, value=5.0, step=0.1)
     radius = calculate_radius(velocity, acceleration)
     st.write(f"Radius of the Loop: {radius:.2f} m")
 
 elif calculation_type == "Centripetal Force":
-    st.latex(r"F_c = m \cdot \frac{v^2}{r}")
+    st.latex(r"F_c = m \cdot \frac{{v_T}^2}{r}")
     radius = st.sidebar.number_input("Radius (m)", min_value=0.1, value=5.0, step=0.1)
     velocity = st.sidebar.number_input("Tangential Velocity (m/s)", min_value=0.0, value=5.0, step=0.1)
     mass = st.sidebar.number_input("Mass (kg)", min_value=0.1, value=1.0, step=0.1)
@@ -84,11 +84,11 @@ elif calculation_type == "Normal/Tension Force":
     mass = st.sidebar.number_input("Mass (kg)", min_value=0.1, value=1.0, step=0.1)
 
     if loop_position == "Bottom of the Loop":
-        st.latex(r"F_N = m \left( \frac{v^2}{r} + g \right)")
+        st.latex(r"F_N = m \left( \frac{{v_T}^2}{r} + g \right)")
         force = calculate_normal_force_bottom(mass, velocity, radius, g)
         st.write(f"Normal/Tension Force at the Bottom: {force:.2f} N")
     elif loop_position == "Top of the Loop":
-        st.latex(r"F_N = m \left( \frac{v^2}{r} - g \right)")
+        st.latex(r"F_N = m \left( \frac{{v_T}^2}{r} - g \right)")
         force = calculate_normal_force_top(mass, velocity, radius, g)
         st.write(f"Normal/Tension Force at the Top: {force:.2f} N")
 
